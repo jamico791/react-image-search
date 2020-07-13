dda386d6611fc15e09a1741c77097810d3491e46
John Amico <john@pop-os.localdomain>
2020-07-13 17:57:17 -0400

    Updated App

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("")
-  const [loading, setLoading] = useState(false)
-  const [images, setImages] = useState([])
-  const API_URL = 'https://nature-image-api.now.sh/search?q='
-  
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value)  
-  }
+	const [searchTerm, setSearchTerm] = useState('')
+	const [loading, setLoading] = useState(false)
+	const [images, setImages] = useState([])
+	const API_URL = 'https://nature-image-api.now.sh/search?q='
 
-  const handleSubmit = async (event) => {
-    event.preventDefault()
-    setLoading(true)
-    setImages([])
-    const response = await fetch(`${API_URL}${searchTerm}`)
-    const json = await response.json()
-    setImages(json.images.map(item => <img key={item.title} src={item.image} alt={searchTerm} />))
-    setLoading(false)
-  }
+	const handleChange = (event) => {
+		setSearchTerm(event.target.value)
+	}
+
+	const handleSubmit = async (event) => {
+		event.preventDefault()
+		setLoading(true)
+		setImages([])
+		const response = await fetch(`${API_URL}${searchTerm}`)
+		const json = await response.json()
+		setImages(
+			json.images.map((item) => (
+				<img key={item.title} src={item.image} alt={searchTerm} />
+			))
+		)
+		setLoading(false)
+	}
 
 	return (
 		<div className="App">
@@ -30,19 +34,19 @@ function App() {
 					className="u-full-width"
 					type="text"
 					id="searchTerm"
-          name="searchTerm"
-          onChange={handleChange}
+					name="searchTerm"
+					onChange={handleChange}
 				/>
 				<button type="submit">Search</button>
 			</form>
-			{loading && <img
-				id="loadingImage"
-				src="https://i.imgur.com/LVHmLnb.gif"
-				alt="loading..."
-			/>}
-			<section className="images">
-        {images}
-			</section>
+			{loading && (
+				<img
+					id="loadingImage"
+					src="https://i.imgur.com/LVHmLnb.gif"
+					alt="loading..."
+				/>
+			)}
+			<section className="images">{images}</section>
 		</div>
 	)
 }
