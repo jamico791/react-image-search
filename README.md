6db760575c6f8f57f63755c3206491113cb0fb1f
John Amico <john@pop-os.localdomain>
2020-07-13 17:19:00 -0400

    Completed App

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # React Image Search
 
 Attempt to re-create CJ from Coding Garden's nature image search in react from scratch
+
+* [x] Setup
+* [x] User Input, DOM Events
+  * [x] Retrieve the search term from the input when the form is submitted
+* [x] Conditional Rendering
+  * [x] Show loading image when form is submitted
+* [x] API Requests
+  * [x] Request the images from the API with the given search term
+* [x] Rendering Lists of Data
+  * [x] Append the API results to the page
+* [x] Conditional Rendering
+  * [x] Hide loading image
--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [searchTerm, setSearchTerm] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [images, setImages] = useState([])
+  const API_URL = 'https://nature-image-api.now.sh/search?q='
+  
+  function handleChange(event) {
+    setSearchTerm(event.target.value)  
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    setLoading(true)
+    const response = await fetch(`${API_URL}${searchTerm}`)
+    const json = await response.json()
+    setImages(json.images.map(item => <img key={item.title} src={item.image} alt={searchTerm} />))
+    setLoading(false)
+  }
+
+	return (
+		<div className="App">
+			<h1>React Image Search</h1>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="searchTerm">Search Term</label>
+				<input
+					className="u-full-width"
+					type="text"
+					id="searchTerm"
+          name="searchTerm"
+          onChange={handleChange}
+				/>
+				<button type="submit">Search</button>
+			</form>
+			{loading && <img
+				id="loadingImage"
+				src="https://i.imgur.com/LVHmLnb.gif"
+				alt="loading..."
+			/>}
+			<section className="images">
+        {images}
+			</section>
+		</div>
+	)
 }
 
-export default App;
+export default App
